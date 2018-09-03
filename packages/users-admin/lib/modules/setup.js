0da3a6f11c06d521a600533def8f352158ecc4f6
Eric Burel <e.burel.contact@gmail.com>
2018-09-03 14:45:25 +0200

    allow username edition

--- packages/users-admin/lib/modules/setup.js
+++ packages/users-admin/lib/modules/setup.js
@@ -4,30 +4,14 @@ import { registerMenuItem } from "meteor/menu";
 
 setupCollectionAdminPages(Users, {
   list: {
-    basicColumns: ["_id", "createdAt", "email", "isAdmin", "groups"]
+    basicColumns: ["_id", "createdAt", "username", "email", "isAdmin", "groups"]
   },
   details: {
-    fields: [
-      "_id",
-      "createdAt",
-      "isAdmin",
-      "displayName",
-      "username",
-      "email",
-      "groups"
-    ]
+    fields: ["_id", "createdAt", "username", "email", "isAdmin", "groups"]
   },
   form: {
     newFields: ["username", "email", "groups", "isAdmin"],
-    editFields: [
-      "_id",
-      "createdAt",
-      "username",
-      "email",
-      "displayName",
-      "groups",
-      "isAdmin"
-    ]
+    editFields: ["_id", "createdAt", "username", "email", "groups", "isAdmin"]
   }
 });
 registerMenuItem("users", { label: "Users", path: "/users" });
