da3be88dc374b2d6f2563f6e378b545fe9a58a83
Eric Burel <e.burel.contact@gmail.com>
2018-09-12 17:40:13 +0200

    clean up home page

--- packages/core/lib/components/Home.jsx
+++ packages/core/lib/components/Home.jsx
@@ -7,22 +7,47 @@ import {
 import homeContent from "../static-content/en/home.js";
 import { FormattedMessage } from "meteor/vulcan:i18n";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 
+const styles = {
+  imgWrapper: {
+    textAlign: "center"
+  },
+  img: {
+    maxWidth: "100%"
+  }
+};
 class Home extends PureComponent {
   render() {
-    const { currentUser } = this.props;
+    const { classes, currentUser } = this.props;
     return (
-      <div>
-        {homeContent && <Components.Markdown source={homeContent} />}
-        {!currentUser && (
-          <div>
-            <Typography variant="headline">
-              <FormattedMessage id="core.signupOrLogin" />
-            </Typography>
-            <Components.AccountsLoginForm />
-          </div>
-        )}
-      </div>
+      <Grid container>
+        <Grid item md={6} sm={12} className={classes.imgWrapper}>
+          <img
+            className={classes.img}
+            src="/img/awesome-vulcan-logo_512.png"
+            alt="Awesome Vulcan Logo"
+          />
+        </Grid>
+        <Grid item md={6} sm={12}>
+          {homeContent && <Components.Markdown source={homeContent} />}
+          {!currentUser ? (
+            <div>
+              <Typography variant="headline">
+                <FormattedMessage id="core.signupOrLogin" />
+              </Typography>
+              <Components.AccountsLoginForm />
+            </div>
+          ) : (
+            <div>
+              <Typography variant="headline">
+                <FormattedMessage id="core.home.loggedIn" />
+              </Typography>
+            </div>
+          )}
+        </Grid>
+      </Grid>
     );
   }
 }
@@ -30,7 +55,7 @@ class Home extends PureComponent {
 registerComponent({
   name: "Home",
   component: Home,
-  hocs: [withCurrentUser]
+  hocs: [withCurrentUser, [withStyles, styles]]
 });
 
 export default Home;
--- packages/core/lib/i18n/en_EN.js
+++ packages/core/lib/i18n/en_EN.js
@@ -1,5 +1,6 @@
 import { addStrings } from "meteor/vulcan:core";
 
 addStrings("en", {
-  "core.signupOrLogin": "Sign up or Sign in"
+  "core.signupOrLogin": "Sign up or Sign in",
+  "core.home.loggedIn": "Now you are logged in, you can proposing resources."
 });
