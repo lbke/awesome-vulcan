6ac85ce50df2feea7c5008a9ed210d730e1ff3fc
Eric Burel <e.burel.contact@gmail.com>
2018-09-13 12:03:14 +0200

    improve i18n

--- packages/core/lib/i18n/fr_FR.js
+++ packages/core/lib/i18n/fr_FR.js
@@ -3,5 +3,7 @@ import { addStrings } from "meteor/vulcan:core";
 addStrings("fr", {
   "core.signupOrLogin": "Connexion/Inscription",
   "core.home.loggedIn":
-    "Vous êtes connecté ! Proposez de nouvelles ressources via le menu gauche."
+    "Vous êtes connecté ! Proposez de nouvelles ressources via le menu gauche.",
+  // vulcan-material-ui
+  "common.load_more": "Voir plus"
 });
