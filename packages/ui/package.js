5ec4fbad312c2c0756199865720ceae09529b375
Eric Burel <e.burel.contact@gmail.com>
2018-10-22 21:24:43 +0200

    fix regression

--- packages/ui/package.js
+++ packages/ui/package.js
@@ -6,4 +6,5 @@ Package.onUse(api => {
   api.use(["vulcan:core", "vulcan:more-material-ui"]);
 
   api.mainModule("lib/client/main.js", "client");
+  api.mainModule("lib/server/main.js", "server");
 });
